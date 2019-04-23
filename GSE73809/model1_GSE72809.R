50a111b5ed46687673847fadf46f31d17b434443
Patrick Hedley-Miller <patrickhedley-miller@PatrickllersMBP.home>
2019-04-23 20:58:29 +0100

    bad roc

--- GSE73809/model1_GSE72809.R
+++ GSE73809/model1_GSE72809.R
@@ -1,26 +1,28 @@
 library(Biobase)
 library(GEOquery)
-library(ggplot2)
-library(tidyr)
-# library(rsample)
-library(caret)
-# library(h2o)
+library(glmnet)
 library(dplyr)
-library(tidyverse)  # data manipulation and visualization
-library(modelr)     # provides easy pipeline modeling functions
+library(ROCR)
+library(dplyr)
+# library(ggplot2)
+# library(tidyr)
+# library(rsample)
+# library(caret)
+# library(h2o)
+# library(tidyverse)  # data manipulation and visualization
+# library(modelr)     # provides easy pipeline modeling functions
 # library(broom)
-library(glmnet)   # implementing regularized regression approaches
 
 ## GET DATA FROM GEO
-gset <- getGEO('GSE72809')
-head(gset)
-saveRDS(gset, file = 'gset_GSE72809')
+# gset <- getGEO('GSE72809')
+# head(gset)
+# saveRDS(gset, file = 'gset_GSE72809')
 
 ## LOAD DATA FROM rds object
-# setwd('/Users/patrickhedley-miller/code/R/infxRNAseq')
-# getwd()
-# gset <-readRDS(file = "gset_GSE72809")
-# head(gset)
+setwd('/Users/patrickhedley-miller/code/R/infxRNAseq')
+getwd()
+gset <-readRDS(file = "gset_GSE72809")
+head(gset)
 # exprs(gset_t[[1]])
 n <- ncol(gset[[1]])
 
@@ -67,7 +69,7 @@ gset_binary_t <- as.data.frame(t(gset_binary))
 dim(gset_binary)
 dim(gset_binary_t)
 
-## CONSTRUCTS GROUND TRUTH LIST 
+## CONSTRUCTS GROUND TRUTH LIST
 truth <- character(0)
 index_binary <- list(bct.index, vrl.index)
 for (i in 1:length(index_binary)){
@@ -87,6 +89,57 @@ gset_binary_t$label <- label
 gset_binary_t[1:5, 47320:47324]
 
 dim(gset_binary_t)
+
+
+## DEFINE TRAINING AND TEST SETS
+x <- gset_binary_t[,(-ncol(gset_binary_t))]
+# mini version
+x_min <- x[,1:30]
+x_min$label <- y
+y <- gset_binary_t[,ncol(gset_binary_t)]
+# y_min <- y[1:10]
+
+train = sample(1:nrow(x_min), round(nrow(x_min)*0.7))
+test = (-train)
+x_train <- x_min[train,]
+dim(x_train)
+x_test <- x_min[test,]
+dim(x_test)
+ytest = y[test]
+
+# logistic
+logistic.mod1 <- glm(label ~., family = "binomial", data = x_train)
+logistic.mod1
+
+
+log.pred <- predict(logistic.mod1, x_test, type = 'response')
+cat.pred <- ifelse(log.pred > 0.5, 0, 1)
+
+
+# classification
+
+list(
+  table(ytest, cat.pred) # %>% prop.table() %>% round(3)
+  # table(y.cat.test, r.class) %>% prop.table() %>% round(3)
+)
+
+par(mfrow=c(1, 2))
+
+prediction(cat.pred, ytest) %>%
+  performance(measure = "tpr", x.measure = "fpr") %>%
+  plot()
+
+
+# model 1 AUC
+prediction(cat.pred, ytest) %>%
+  performance(measure = "auc") %>%
+  .@y.values
+
+
+
+
+
+
 
 
 ## TRAIN TEST SPLIT
@@ -110,34 +163,6 @@ print(paste('bacterial test proportion:', test_bac_prop))
 
 
 
-# Create training and testing feature model matrices and response vectors.
-# we use model.matrix(...)[, -1] to discard the intercept
-gset_train_x <- model.matrix(label ~ ., train_1)[,-1]
-gset_train_y <- train_1$label
-
-gset_test_x <- model.matrix(label ~ ., test_1)[,-1]
-gset_test_y <- test_1$label
-
-# What is the dimension of of your feature matrix?
-dim(gset_train_x)
-
-
-gset_ridge <- glmnet(
-  x = gset_train_x,
-  y = gset_train_y,
-  alpha = 0
-)
-
-plot(gset_ridge, xvar = "lambda")
-
-
-
-
-
-
-
-
-
 
 
 
@@ -148,7 +173,7 @@ features <- setdiff(names(train_1), 'truth')
 # pre-process estimation based on training features
 pre_process <- preProcess(
   x      = train_1[, features],
-  method = c("center", "scale")    
+  method = c("center", "scale")
 )
 
 # apply to both training & test
@@ -158,24 +183,3 @@ test_x  <- predict(pre_process, test_1[, features])
 train_x[1:5, 47320:47323]
 test_x[1:5, 1:5]
 # remove(train_x, test_x)
-
-## LOAD DATA FROM rds object
-# saveRDS(train_x, file = 'train_x_GSE72809')
-# saveRDS(test_x, file = 'test_x_GSE72809')
-train_x <- readRDS(file = 'train_x_GSE72809')
-test_x <- readRDS(file = 'test_x_GSE72809')
-
-train_x$truth <- truth
-
-model1 <- glm(truth ~ ILMN_1657871, family = "binomial", data = train_1)
-summary(model1)
-
-pred <- predict(model1, test_1, type='response')
-
-pred
-pred.class <- ifelse(pred > 0.5, 1, 0)
-pred.class
-
-test_1
-
-
