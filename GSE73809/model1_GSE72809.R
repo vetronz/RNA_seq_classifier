4eb86058171fc0d7e9ff55b6655b3511b640b067
Patrick Hedley-Miller <patrickhedley-miller@dyn910-241.guest.ic.ac.uk>
2019-04-24 16:32:44 +0100

    logistic regression cracked

--- GSE73809/model1_GSE72809.R
+++ GSE73809/model1_GSE72809.R
@@ -4,7 +4,7 @@ library(glmnet)
 library(dplyr)
 library(ROCR)
 library(dplyr)
-# library(ggplot2)
+library(ggplot2)
 # library(tidyr)
 # library(rsample)
 # library(caret)
@@ -27,8 +27,8 @@ head(gset)
 n <- ncol(gset[[1]])
 
 
-gset_df <- exprs(gset[[1]])
-gset_df[1:5,1:5]
+gset.df <- exprs(gset[[1]])
+gset.df[1:5,1:5]
 
 colnames(phenoData(gset[[1]]))
 phenoData(gset[[1]])$'category:ch1'
@@ -51,83 +51,121 @@ for (i in 1:n){
 vrl.index <- vrl.index[!is.na(vrl.index)]
 
 
-# bct_df <- gset_df[, bct]
-# vrl_df <- gset_df[, vrl]
-# bct_df <- gset.sig[, bct]
-# vrl_df <- gset.sig[, vrl]
-# dim(bct_df)
-# dim(vrl_df)
 
 ## SUBSET THE GSET TO GET BACT VIRAL DF
-index_binary <- c(bct.index, vrl.index)
-gset_binary <- gset_df[,index_binary]
-gset_binary_t <- as.data.frame(t(gset_binary))
+index.binary <- c(bct.index, vrl.index)
+gset.binary <- gset_df[,index.binary]
+gset.binary.t <- as.data.frame(t(gset.binary))
 
-# gset_binary[1:5,1:5]
-# gset_binary_t[1:5,1:5]
-
-dim(gset_binary)
-dim(gset_binary_t)
+# dim(gset_binary)
+dim(gset.binary.t)
 
 ## CONSTRUCTS GROUND TRUTH LIST
 truth <- character(0)
-index_binary <- list(bct.index, vrl.index)
-for (i in 1:length(index_binary)){
-  for (j in 1:length(index_binary[[i]])){
+index.binary <- list(bct.index, vrl.index)
+index.binary
+for (i in 1:length(index.binary)){
+  for (j in 1:length(index.binary[[i]])){
     print(paste(i, j))
     truth = append(truth,i)
   }
 }
-truth
 
-# yyz$b <- as.numeric(as.character(yyz$b))
+label <- factor(truth, levels = c(1,2), labels = c('bacterial', 'viral'))
+label
 
-truth_num <- as.numeric(truth)-1
-label <- as.factor(truth_num)
+attributes(label)
 
-gset_binary_t$label <- label
-gset_binary_t[1:5, 47320:47324]
-
-dim(gset_binary_t)
+gset.binary.t$label <- label
+gset.binary.t[1:5, 47320:47324]
+gset.binary.t[1:5, c(1,2,47323, 47324)]
+dim(gset.binary.t)
 
 
 ## DEFINE TRAINING AND TEST SETS
-x <- gset_binary_t[,(-ncol(gset_binary_t))]
-# mini version
-x_min <- x[,1:30]
-x_min$label <- y
-y <- gset_binary_t[,ncol(gset_binary_t)]
-# y_min <- y[1:10]
+x <- gset.binary.t[, c(1,2,47323, 47324)]
+dim(x)
+# x$label <- y
+y <- gset.binary.t[,ncol(gset.binary.t)]
 
-train = sample(1:nrow(x_min), round(nrow(x_min)*0.7))
-test = (-train)
-x_train <- x_min[train,]
+# n <- nrow(x)
+# index <- seq(1:n)
+# train = sample(1:n, round(n*0.8))
+# test = index[-train]
+# 
+# # intersect(train, test)
+# # integer(0)
+# 
+x_train <- x[train,]
 dim(x_train)
-x_test <- x_min[test,]
+x_test <- x[test,][-ncol(x)]
 dim(x_test)
-ytest = y[test]
+# ytest = y[test]
+
+
+# Basic scatter plot
+# x_test <- x[test,]
+p <- ggplot(x_train, aes(x=ILMN_1343291, y=ILMN_3311190))
+# p <- ggplot(x_train, aes(x=ILMN_1343291, y=ILMN_1343295))
+p + geom_point(aes(colour = factor(label)), size = 2)
+
 
 # logistic
 logistic.mod1 <- glm(label ~., family = "binomial", data = x_train)
-logistic.mod1
-
+summary(logistic.mod1)
+# logistic.mod1
 
 log.pred <- predict(logistic.mod1, x_test, type = 'response')
-cat.pred <- ifelse(log.pred > 0.5, 0, 1)
+cat.pred <- ifelse(log.pred < 0.5, 'bact', 'viral')
+cat.pred
 
+# x_test['GSM1872559',]
+# 
+# x_test['GSM1872497',]
+# 
+# x_test['GSM1872495',]
+
+
+
+
+x_train[1,]
+logistic.mod1$coefficients
+log.pred[1]
+
+(0.5562*4.9273 + 5.898*-1.12124)+4.5931
+a <- 0.556119*4.927301
+b <- -1.121235 * 5.897797
+c <- 4.593186
 
 # classification
-
 list(
   table(ytest, cat.pred) # %>% prop.table() %>% round(3)
   # table(y.cat.test, r.class) %>% prop.table() %>% round(3)
 )
 
-par(mfrow=c(1, 2))
 
-prediction(cat.pred, ytest) %>%
+numeric.truth <- ifelse(truth == '1', 0, 1)
+dev.off(dev.list()["RStudioGD"])
+
+prediction(log.pred, ytest) %>%
   performance(measure = "tpr", x.measure = "fpr") %>%
   plot()
+
+
+
+## HAND MADE PREDICTIONS
+logistic.mod1$coefficients
+x_test[1,]
+
+logistic.mod1$coefficients[-1] * x_test[1,]
+sum(logistic.mod1$coefficients[-1] * x_test[1,])
+
+
+length(x_test[1,][-length(x_test)])
+length(logistic.mod1$coefficients[-1])
+
+x_test[1,][-length(x_test)] * logistic.mod1$coefficients[-1]
+sum(x_test[1,][-length(x_test)] * logistic.mod1$coefficients[-1]) + logistic.mod1$coefficients[1]
 
 
 # model 1 AUC
