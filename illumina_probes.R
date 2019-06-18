fc4c99007da1f18a2c4a09a29d731864bfc50c81
Patrick Hedley-Miller <patrickhedleymiller@gmail.com>
2019-06-18 15:41:28 +0100

    andrew lapply mystery

--- illumina_probes.R
+++ illumina_probes.R
@@ -5,28 +5,86 @@ illumina <- read.table('ill_probe.csv', sep = ',', stringsAsFactors = FALSE, fil
 head(illumina)
 nrow(illumina)
 
-module.assign[module.assign == 2]
-# 5720482 2570300 2100196  990768 3360343
-trans <- c(5720482, 2570300, 2100196,  990768, 3360343)
-trans <- c(7650358, 5720482, 2570300, 3180392, 5090754)
-trans <- c(830440)
-which(illumina$Array_Address_Id %in% trans)
+getwd()
+setwd('/home/patrick/Code/R')
+# setwd('/Users/patrickhedley-miller/code/gitWorkspace/infxRNAseq')
+
+# rm(list=setdiff(ls(), 'all'))
+load('esets.RData')
+
+
+# trans <- c(7650358, 5720482, 2570300, 3180392, 5090754)
+# sum(illumina$Array_Address_Id %in% trans)
+
+probe.id <- illumina$Probe_Id
+
 
 probeID <- illumina$Probe_Id[which(illumina$Array_Address_Id %in% trans)]
 
-# 830440                     STAM binding protein like 1 ENSG00000138134
+df.1$Gene <- as.character(df.1$Gene)
 
-x <- illuminaHumanv4CHR
-x <- illuminaHumanv4NUID
-x <- illuminaHumanv4ALIAS2PROBE
-x <- illuminaHumanv4ENSEMBL
-x <- illuminaHumanv4GENENAME
-x <- illuminaHumanv4GO
-x <- illuminaHumanv4MAP
-x <- illuminaHumanv4REFSEQ
-data.frame(Gene=unlist(mget(x = probeID, envir = x)))
+conversion <- illuminaHumanv4ENSEMBL
+# conversion <- illuminaHumanv4GENENAME
+# x <- illuminaHumanv4CHR
+# x <- illuminaHumanv4NUID
+# x <- illuminaHumanv4ALIAS2PROBE
+# x <- illuminaHumanv4GO
+# x <- illuminaHumanv4MAP
+# x <- illuminaHumanv4REFSEQ
+
+con <- mget(x = probe.id, envir = conversion)
+
+lapply(con[670:680], function(x){
+  paste0('the probe is: ', x)
+})
+
+unlist(lapply(con, function(x){x[1]}))
+unlist(lapply(con, function(x){x[2]}))
+con[1:100]
+illumina$ensemb <- as.character(con)
 
 
+lapply(con, function(x){
+  x[1]
+}) %>% unlist
+lapply(con, function(x){
+  if(length(x)>=2){
+    x[2]
+  } else {
+    NA
+  }}) %>% unlist
+
+
+
+get_element <- function(l, n){
+  lapply(l, function(elem, n){
+    if(length(elem)>=n){
+      elem[n]
+    } else {
+      NA
+    }}, n) %>% unlist  
+}
+
+
+View(illumina)
+
+ENSG00000137959
+
+df.1 <- data.frame(Gene=unlist(mget(x = probeID, envir = x)))
+View(df.1)
+
+
+# test 
+# ILMN_1770772
+# ENSG00000092009
+df.1[which(rownames(df.1) == "ILMN_1770772"),]
+rownames(df.1)[which(df.1$Gene == 'ENSG00000092009')]
+
+rownames(df.1)[which(df.1$Gene == 'ENSG00000137959')]
+# "ILMN_1835092" "ILMN_1723912"
+
+df.1[which(rownames(df.1) == "ILMN_1835092"),]
+df.1[which(rownames(df.1) == "ILMN_1723912"),]
 
 # ILMN_1729749 3180392 ENSG00000138646
 # Q9UII4 (HERC5_HUMAN)
